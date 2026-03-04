3a8f44a807822f29aba916091c4fc1af813b5db9
Oho Too <itmoho2025@gmail.com>
2026-03-04 18:12:52 +0300

    Add operator -, scalar * and different small adds

--- matrix.cpp
+++ matrix.cpp
@@ -28,18 +28,6 @@ public:
         return matrix[i];
     }
 
-
-
-    void print() {
-        for(const auto& line : matrix) {
-            cout << "| ";
-            for(const float& num : line) {
-                cout << num << " ";
-            }
-            cout << "|" << endl;
-        }
-    }
-
     Matrix operator+(const Matrix& other) const {
         if((matrix.size() != other.matrix.size()) or (matrix[0].size() != other.matrix[0].size())) {
             throw invalid_argument("Size!");
@@ -57,8 +45,100 @@ public:
         return final;
     }
 
+    Matrix operator-(const Matrix& other) const {
+        if((matrix.size() != other.matrix.size()) or (matrix[0].size() != other.matrix[0].size())) {
+            throw invalid_argument("Size!");
+        }
+
+        Matrix final(matrix.size(), matrix[0].size());
+        
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++) {
+                final[i][j] = matrix[i][j] - other.matrix[i][j];
+            }
+        }
+
+        return final;
+    }
+
+
+    Matrix operator*(const Matrix& other) const {
+        if(columns == other.matrix.size()) {
+
+            Matrix temporary(lines, other.matrix[0].size());
+
+            for(int i = 0; i < lines; i++) {
+                for(int j = 0; j < other.matrix[0].size(); j++) {
+                    for(int k = 0; k < columns; k++) {
+                        temporary[i][j] += matrix[i][k] * other.matrix[k][j];
+                    }
+                }
+            }
+            return temporary;
+
+        }
+        else {
+            throw invalid_argument("Size!");
+        }
+    }
+
+    //!---------------------------------------------------------------------------------------
+
+    Matrix get_T() const{
+        Matrix trans(columns, lines);
+        for(int i = 0; i < lines; i++) {
+            for(int j = 0; j < columns; j++) {
+                trans[j][i] = matrix[i][j];
+            }
+        }
+        return trans;
+    }
+
+    void T() {
+        // Создаем новую матрицу с переставленными размерами
+        Matrix new_matrix(columns, lines);
+        
+        // Заполняем
+        for(int i = 0; i < lines; i++) {
+            for(int j = 0; j < columns; j++) {
+                new_matrix[j][i] = matrix[i][j];
+            }
+        }
+        
+        // Заменяем
+        matrix = new_matrix.matrix;
+        
+        // Меняем размеры
+        swap(lines, columns);
+    }
+
+
+
+
+
+
+
+
+
+
+    //!---------------------------------------------------------------------------------------
+
+
+    void print() {
+        for(const auto& line : matrix) {
+            cout << "| ";
+            for(const float& num : line) {
+                cout << num << " ";
+            }
+            cout << "|" << endl;
+        }
+        cout << endl;
+    }
+
     void fill( const std::vector<float>& nums ) {
-        /*if(columns*lines != nums.size()) {
+        /*
+        if(columns*lines != nums.size()) {
             throw invalid_argument("Size!");
         }*/
         for(int i = 0; i < lines; i++){
@@ -66,6 +146,16 @@ public:
                 matrix[i][j] = nums[i*columns+j];
             }
         } 
+    }
+
+    void fill_random() {
+
+        for(int i=0; i < lines; i++) {
+            for(int j=0; j < columns; j++) {
+                matrix[i][j] = (float)rand() / RAND_MAX; //todo Чёт кринж с этим (float) надо думать
+            }
+        }
+
     }
 };
 
@@ -80,45 +170,19 @@ std::vector<std::vector<float>> matrix(int lines, int columns) {
 
 int main() {
 
-    srand(time(nullptr));
+    Matrix mat1(3, 3);
+    Matrix mat2(3, 5);
 
-
-    Matrix mat(5, 5);
-    
-    std::vector<float> nums(25, 0);
-
-    for(int i=0; i < nums.size(); i++) {
-        nums[i] = rand() % 11;
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-
-    
-    mat.fill(nums);
-
-
-    mat.print();
-
-    cout << endl;
-
-
-    Matrix mat1(5, 5);
-
-
-    for(int i=0; i < nums.size(); i++) {
-        nums[i] = rand() % 11;
-        cout << nums[i] << " ";
-    }
-    cout << endl;
-
-    mat1.fill(nums);
+    mat1.fill_random();
+    mat2.fill_random();
 
     mat1.print();
+    mat2.print();
 
-    cout << endl << endl;
+    (mat1*mat2).print();
 
-    mat = mat+mat1;
-    mat.print();
+
+
 
     return 0;
 }
